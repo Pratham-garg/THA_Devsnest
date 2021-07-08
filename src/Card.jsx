a38759284553a234a45fb15ef744b92470d84d4d
Pratham-garg <61599226+Pratham-garg@users.noreply.github.com>
2021-07-08 13:20:19 +0530

    not

--- src/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Card.css';
-function Card(props){
-    return(
-        <div className="box"><img src={`${props.name}`} alt="meme"/></div>
-    );
-}
-export default Card;
